8090f1f64ccea3edf80b4b7e6c0181db5ca22587
ps20221093 <46880377+ps20221093@users.noreply.github.com>
2019-02-25 13:10:46 +0800

    Add files via upload

---
+++ Button's point and click adventure/Pretty Not Easy/Level 1/sketch.js
@@ -0,0 +1,31 @@
+var b1;
+var b2;
+
+function setup()
+{
+	createCanvas(500,500);
+	b1 = new Button(10,10,100,25);
+	b1.setText("Easy");
+	b1.setTextOver("GO");
+	b1.setButtonOverFill(255,255,50);
+
+	b2 = new Button(10,100,100,25);
+
+}
+
+function draw()
+{
+	background(125,125,125);
+
+	b1.showButton();
+	b2.showButton();
+
+	if (b1.getButtonState() == 1)
+	{
+		window.open("Easy/Easy.html","_self");
+	}
+	if (b2.getButtonState() == 1)
+	{
+		window.open("../buttonList.html","_self");
+	}
+}
---
+++ Button's point and click adventure/Pretty Not Easy/sketch.js
@@ -0,0 +1,32 @@
+var b1;
+var b2;
+
+function setup()
+{
+	createCanvas(500,500);
+	b1 = new Button(10,10,100,25);
+	b1.setText("Level 1");
+	b1.setTextOver("GO");
+	b1.setButtonOverFill(255,255,50);
+
+	b2 = new Button(10,100,100,25);
+	b2.setText("Main Menu thanks dad")
+
+}
+
+function draw()
+{
+	background(125,125,125);
+
+	b1.showButton();
+	b2.showButton();
+
+	if (b1.getButtonState() == 1)
+	{
+		window.open("Level 1/Level 1 Dad.html","_self");
+	}
+	if (b2.getButtonState() == 1)
+	{
+		window.open("../buttonList.html","_self");
+	}
+}
